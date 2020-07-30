2edb2df36646974958d77b7d510246ea25a1dab4
realyoungk <9725394@naver.com>
2020-07-30 15:33:05 +0900

    #3.3 sendMail Function with Mailgun

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,37 @@
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(__dirname, ".env") });
+
 import { adjectives, nouns } from "./word";
+import nodemailer from "nodemailer";
+import mgTransport from "nodemailer-mailgun-transport";
+import jwt from "jsonwebtoken";
 
 export const generateSecret = () => {
   const randomNumber = Math.floor(Math.random() * adjectives.length);
   return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
 };
+
+export const sendMail = (email) => {
+  const options = {
+    auth: {
+      domain: process.env.MAILGUN_DOMAIN,
+      api_key: process.env.MAILGUN_API,
+    },
+  };
+
+  const client = nodemailer.createTransport(mgTransport(options));
+  return client.sendMail(email);
+};
+
+export const sendSecretMail = (address, secret) => {
+  const email = {
+    from: "9725394@naver.com",
+    to: address,
+    subject: "🔒Login Secret for Prismagram🔒",
+    html: `Hello! Your login secret is <strong> ${secret} </strong>.<br/> Copy paste on the app/website to log in`,
+  };
+  return sendMail(email);
+};
+
+export const generateToken = (id) => jwt.sign({ id }, process.env.JWT_SECRET);
