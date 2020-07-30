18818fbe4e00a9770439f0fb72819c3c0784ee58
realyoungk <9725394@naver.com>
2020-07-30 18:05:12 +0900

    #3.6 Passport JWT part Three

--- src/passport.js
+++ src/passport.js
@@ -7,10 +7,10 @@ import { Strategy, ExtractJwt } from "passport-jwt";
 import { prisma } from "../generated/prisma-client";
 
 const jwtOptions = {
-  // 토큰을 받아와서 해석
-  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
   //토큰을 암호화하는거
   secretOrKey: process.env.JWT_SECRET,
+  // 토큰을 받아와서 해석
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
 };
 
 const verifyUser = async (payload, done) => {
@@ -25,6 +25,14 @@ const verifyUser = async (payload, done) => {
     return done(error, false);
   }
 };
+
+export const authenticateJwt = (req, res, next) =>
+  passport.authenticate("jwt", { session: false }, (error, user) => {
+    if (user) {
+      req.user = user;
+    }
+    next();
+  })(req, res, next);
 
 //해석된 정보를 콜백함수로 전달
 passport.use(new Strategy(jwtOptions, verifyUser));
--- src/server.js
+++ src/server.js
@@ -1,20 +1,21 @@
-import dotenv from "dotenv";
-import path from "path";
-dotenv.config({ path: path.resolve(__dirname, ".env") });
-
+import "./env";
 import { GraphQLServer } from "graphql-yoga";
+import { prisma } from "../generated/prisma-client";
 import logger from "morgan";
-import passport from "passport";
 import schema from "./schema";
-import "./utils";
 import "./passport";
+import { authenticateJwt } from "./passport";
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 
-const server = new GraphQLServer({ schema });
+const server = new GraphQLServer({
+  schema,
+  // 리졸버들간 정보를 공유할때 사용
+  context: ({ request }) => ({ request }),
+});
 
 server.express.use(logger("dev"));
-// 파일 경로 보호
-server.express.use("/api", passport.authenticate("jwt"));
+
+server.express.use(authenticateJwt);
 
 server.start({ port: PORT }, () => console.log(`✔Server Running PORT : http://localhost:${PORT}✔`));
